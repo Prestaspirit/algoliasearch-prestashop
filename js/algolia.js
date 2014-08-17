4d3f7aaf1be8b782c7c5c19342df75ad38e4d3e3
Francois Gaillard <francois.gaillard@prestashop.com>
2014-08-17 19:43:38 +0200

    Layout fixed (front)

--- js/algolia.js
+++ js/algolia.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	var algolia = new AlgoliaSearch(algolia_application_id, algolia_search_only_api_key);
 	var index = algolia.initIndex(algolia_index_name);
-	
+
 	var template = Hogan.compile('<div class="hit">' +
 	  '<div class="name">' +
 	  	'<img src="//{{{ image_link }}}" class="algolia-search-image" />' +
@@ -12,15 +12,15 @@ $(document).ready(function() {
 	    '<div class="attribute"><b>{{ attribute }}</b>: {{{ value }}}</div>' +
 	  '{{/matchingAttributes}}' +
 	  '</div>');
-	
-	$('#algolia_search_query_top').typeahead(null, {
+
+	$('#algolia-search-query-top').typeahead(null, {
 	  source: index.ttAdapter({ hitsPerPage: 5 }),
 	  displayKey: 'email',
 	  templates: {
 	    suggestion: function(hit) {
 	      hit.matchingAttributes = [];
 	      for (var attribute in hit._highlightResult) {
-	        if (attribute === 'name' || attribute == 'category') {
+	        if (attribute === 'name' || attribute === 'category') {
 	          continue;
 	        }
 	        if (hit._highlightResult[attribute].matchLevel !== 'none') {
--- views/templates/hook/search.tpl
+++ views/templates/hook/search.tpl
@@ -1,7 +1,7 @@
 <div class="col-sm-4 clearfix">
 	<div id="algolia_search_block_top">
 		<form method="get" action="#" class="row">
-			<input type="text" id="algolia_search_query_top" name="algolia_search_query" class="col-sm-12 typeahead" />
+			<input type="text" id="algolia-search-query-top" name="algolia_search_query" class="col-sm-12 typeahead" />
 		</form>
 	</div>
 </div>
